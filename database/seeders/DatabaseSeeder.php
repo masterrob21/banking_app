dcd69ec6209d66653ef4436a4bfd16f115a64b47
Robert Quainoo <softdeveloperrob@gmail.com>
2023-11-08 16:37:53 +0000

    Created seeder for the gender table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RegionSeeder::class,
             TitleSeeder::class,
+            GenderSeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
